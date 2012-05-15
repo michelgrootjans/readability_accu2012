50f2eb7873ad1a36b88c305d1c27e8993a8cec5d
Michel Grootjans <michel.grootjans@gmail.com>
2012-05-15 16:10:24 +0200

    reset original primes

--- code/lib/primes.rb
+++ code/lib/primes.rb
@@ -1,18 +1,19 @@
-class Fixnum
-  def is_multiple_of? number
-    return self%number == 0 && self != number
+def meets_criteria num
+  criteria = {}
+  (2..num).each do |i|
+    criteria[i] = true
   end
+  (2..(num/2)).each do |i|
+    j = i + i
+    while (j <= num) do
+      criteria[j] = false
+      j = j + i
+    end
+  end
+  criteria.find_all{|c| c[1]}.map{|c| c[0]}
 end
 
-# implementation of the sieve of Erastosthenes
-# http:// wikipedia ...
-def primes_up_to max_value
-
-  prime_candidates = (2..max_value).to_a
-
-  (2..(max_value/2)).each do |prime|
-    prime_candidates.delete_if{|number| number.is_multiple_of?(prime) }
-  end
-
-  prime_candidates
+num = ARGV[0].to_i
+meets_criteria(num).each do |i|
+  puts "#{i} meets criteria"
 end
